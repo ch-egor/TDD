bc33168be087f0a6331da55a8a9db234d2f8c218
ch-egor <ch_egor.92@mail.ru>
2016-04-26 14:58:09 +0700

    add test for ui

--- test/app.test.js
+++ test/app.test.js
@@ -1,1 +1,23 @@
-// TODO write tests
+describe('VirtualFriend', function() {
+  beforeEach(module('VirtualFriend'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+  
+  var $scope, controller;
+  
+  beforeEach(function() {
+    $scope = {};
+    controller = $controller('chatCtrl', { $scope: $scope });
+  });
+  
+  it('ensures that the message has been sent', function() {
+    $scope.messageText = 'test message';
+    $scope.sendMessage();
+    expect($scope.messages[0].from).toEqual('Me');
+    expect($scope.messages[0].text).toEqual('test message');
+  });
+});
