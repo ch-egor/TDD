f646cd5e29f9378465c26d1e640b25ffab558c2a
ch-egor <ch_egor.92@mail.ru>
2016-04-26 14:55:44 +0700

    add simple ui

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,1 +1,20 @@
-// TODO write app code
+var app = angular.module('VirtualFriend', []);
+
+app.factory('friendFactory', function () {
+  function Friend() {
+    // TODO write Friend constructor
+  }
+  
+  Friend.prototype = {
+    // TODO write Friend methods
+  };
+  
+  return Friend;
+});
+
+app.controller('chatCtrl', [
+  '$scope', 'friendFactory',
+  function($scope, Friend) {
+    // TODO write code for controller
+  }
+]);
