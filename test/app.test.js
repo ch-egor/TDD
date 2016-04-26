e24ee51f7f678e96d5088ce60919c80b26b0b22e
ch-egor <ch_egor.92@mail.ru>
2016-04-26 21:17:10 +0700

    add test checking username correctness

--- test/app.test.js
+++ test/app.test.js
@@ -1,23 +1,32 @@
-describe('VirtualFriend', function() {
+describe('VirtualFriend', function () {
   beforeEach(module('VirtualFriend'));
 
-  var $controller;
+  var $controller, $timeout;
 
-  beforeEach(inject(function(_$controller_){
+  beforeEach(inject(function (_$controller_, _$timeout_) {
     $controller = _$controller_;
+    $timeout = _$timeout_;
   }));
-  
+
   var $scope, controller;
-  
-  beforeEach(function() {
+
+  beforeEach(function () {
     $scope = {};
-    controller = $controller('chatCtrl', { $scope: $scope });
+    controller = $controller('chatCtrl', {$scope: $scope});
   });
-  
-  it('ensures that the message has been sent', function() {
+
+  it('ensures that the message has been sent', function () {
     $scope.messageText = 'test message';
     $scope.sendMessage();
     expect($scope.messages[0].from).toEqual('Me');
     expect($scope.messages[0].text).toEqual('test message');
+  });
+
+  it('makes sure the bot gets the username right', function () {
+    $scope.messageText = 'test name';
+    $scope.sendMessage();
+    $timeout.flush();
+    expect($scope.messages[0].from).toEqual('VF');
+    expect($scope.messages[0].text).toMatch('test name');
   });
 });
