a1d21cd0f52bbf2d36b8cab469e5293027fc9288
ch-egor <ch_egor.92@mail.ru>
2016-04-27 10:51:44 +0700

    add 2 more username tests

--- test/app.test.js
+++ test/app.test.js
@@ -15,18 +15,39 @@ describe('VirtualFriend', function () {
     controller = $controller('chatCtrl', {$scope: $scope});
   });
 
-  it('ensures that the message has been sent', function () {
+  it('ensures the message has been sent', function () {
     $scope.messageText = 'test message';
     $scope.sendMessage();
     expect($scope.messages[0].from).toEqual('Me');
     expect($scope.messages[0].text).toEqual('test message');
   });
 
-  it('makes sure the bot gets the username right', function () {
+  it('makes sure the bot gets the username right 1', function () {
     $scope.messageText = 'test name';
     $scope.sendMessage();
     $timeout.flush();
     expect($scope.messages[0].from).toEqual('VF');
     expect($scope.messages[0].text).toMatch('test name');
+  });
+  
+  it('makes sure the bot gets the username right 2', function () {
+    $scope.messageText = "Hey, I'm Bob ";
+    $scope.sendMessage();
+    $timeout.flush();
+    expect($scope.messages[0].from).toEqual('VF');
+    expect($scope.messages[0].text).toMatch('Say again');
+    $scope.messageText = "  I'm Bob   ";
+    $scope.sendMessage();
+    $timeout.flush();
+    expect($scope.messages[0].from).toEqual('VF');
+    expect($scope.messages[0].text).toMatch('Nice to meet you, Bob!');
+  });
+  
+  it('makes sure the bot gets the username right 3', function () {
+    $scope.messageText = 'My name is Hanna.';
+    $scope.sendMessage();
+    $timeout.flush();
+    expect($scope.messages[0].from).toEqual('VF');
+    expect($scope.messages[0].text).toMatch('Nice to meet you, Hanna!');
   });
 });
